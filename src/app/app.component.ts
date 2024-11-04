0a6789f5f4995f990ce89232e008fc69e3359750
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-11-04 20:44:10 -0300

    Cria `ListaCategoriasComponent` e `CategoriaComponent`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ListaCategoriasComponent } from './components/lista-categorias/lista-categorias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    CategoriaComponent,
+    ListaCategoriasComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
